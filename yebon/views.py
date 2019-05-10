a742da1fa7ded07ac2db914547e60d4847dbc413
BbongLee <jebon0722@gmail.com>
2019-05-11 00:23:50 +0900

    웹에 데이터 넣어보기

--- yebon/views.py
+++ yebon/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import HappyThings
 
 # request를 넘겨받아 render메서드 호출, yebon/thing_list.html 템플릿을 보여주는 함수
 def thing_list(request):
-    return render(request, 'yebon/thing_list.html', {})
+	things = HappyThings.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+	return render(request, 'yebon/thing_list.html', {'things':things})
